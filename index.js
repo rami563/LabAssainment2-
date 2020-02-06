0061739212a268f36b7648d8eee927bea665ebac
Your Name <you@example.com>
2020-02-05 16:09:44 -0800

    lab2 done

---
+++ index.js
@@ -0,0 +1,16 @@
+var Greeter = (function () {
+    function Greeter(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Greeter.prototype.showGreeting = function () {
+        var row2 = document.getElementById("r1c2");
+        row2.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+    };
+    Greeter.prototype.resetGreeting = function () {
+        var row2 = document.getElementById("r1c2");
+        row2.innerHTML = 'r1c2';
+    };
+    return Greeter;
+})();
+var greeter = new Greeter('Rami', 'Abed');
